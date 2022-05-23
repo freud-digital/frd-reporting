c655770b150abf312093b89d9cd16de9c4734c5c
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-23 11:24:53 +0200

    set up some basic dashboard with partially hard coded data

---
+++ js/works.js
@@ -0,0 +1,42 @@
+Highcharts.getJSON(
+    '../data/works_hc.json',
+    function (data) {  
+        Highcharts.chart('container_works', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: 0,
+                plotShadow: false
+            },
+            title: '',
+            tooltip: {
+                pointFormat: 'Werke: <b>{point.percentage:.1f}%</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    dataLabels: {
+                        enabled: true,
+                        distance: -50,
+                        style: {
+                            fontWeight: 'bold',
+                            color: 'white'
+                        }
+                    },
+                    startAngle: -90,
+                    endAngle: 90,
+                    center: ['50%', '75%'],
+                    size: '110%'
+                }
+            },
+            series: [{
+                type: 'pie',
+                innerSize: '50%',
+                data: data
+            }]
+        });
+    }
+);
--- umschrift_stats.py
+++ umschrift_stats.py
@@ -9,8 +9,18 @@ with open(UMSCHRIFT_DATA, 'r') as f:
     data = json.load(f)
 
 umschrift = df.groupby(['man_field_status_umschrift'])
-data[time.time()] = {
+data[int(time.time() * 1000)] = {
     key: int(value) for key, value in dict(umschrift.size()).items()
 }
 with open(UMSCHRIFT_DATA, 'w') as f:
     json.dump(data, f, ensure_ascii=True)
+
+with open(UMSCHRIFT_DATA, 'r') as f:
+    data = json.load(f)
+
+data_array = [
+    [float(key), value['2']] for key, value in data.items()
+]
+
+with open(f"{UMSCHRIFT_DATA.replace('.json', '_hc.json')}", 'w') as f:
+    json.dump(data_array, f, ensure_ascii=True)

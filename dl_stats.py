7f4cfcfa0bf1aff92b2d11a52b0211d95f6848ef
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-23 08:48:46 +0200

    created data folder

--- dl_stats.py
+++ dl_stats.py
@@ -2,4 +2,4 @@ import pandas as pd
 from utils import yield_manifestation
 
 df = pd.DataFrame(yield_manifestation())
-df.to_csv('manifestations.csv', index=False)
+df.to_csv('./data/manifestations.csv', index=False)

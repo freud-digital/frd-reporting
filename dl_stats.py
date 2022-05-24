da06c1367740aa82547c02fb448d55532cacf67c
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-24 10:17:29 +0200

    remove manifestations with empty signaturs or reference signatures

--- dl_stats.py
+++ dl_stats.py
@@ -5,4 +5,7 @@ from utils import yield_manifestation
 df = pd.DataFrame(yield_manifestation())
 work_sig_df = pd.read_csv(WORK_SIGNATURS)
 merged = pd.merge(df, work_sig_df, on='werk_signatur_id')
+merged['man_full_sig'] = merged["werk_signatur"] + merged["man_sig"]
+merged = merged[~merged.man_full_sig.str.contains(">", na=False)]
+merged = merged.dropna(subset=['man_full_sig'])
 merged.to_csv(MAN_CSV, index=False)

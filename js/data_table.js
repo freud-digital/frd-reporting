b11f2d406fce904c65e2b241012471b0cfe81926
csae8092 <csae8092@users.noreply.github.com>
2022-05-26 13:43:05 +0000

    deploy: a12841986d09b56a51cf829d42a1c04619dbaa02

---
+++ js/data_table.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    $('#example').DataTable({
+        ajax: 'data/data_table.json',
+        deferRender: true,
+        columns: [{
+                data: 'man_full_sig',
+                render: function (data, type, row) {
+                    if (type === 'display') {
+                        let link = row.man_website_url;
+                        return '<a href="' + link + '">' + data + '</a>';
+                    }
+
+                    return data;
+                },
+            },
+            {
+                data: 'man_title',
+                render: function (data, type, row) {
+                    if (type === 'display') {
+                        let hash_id = row.man_id;
+                        return data + '<br />' + '<small>' + hash_id + '</small>'
+                    }
+                    return data;
+                },
+            },
+            {
+                data: 'man_field_status_umschrift'
+            },
+            {
+                data: 'man_pages'
+            },
+            {
+                data: 'man_chapters'
+            },
+            {
+                data: 'work_title',
+                render: function (data, type, row) {
+                    if (type === 'display') {
+                        let link = row.werk_website_url;
+                        let hash_id = row.work_id;
+                        return '<a href="' + link + '" target="_blank">' + data + '</a><br /><small>' + hash_id + '</small>';
+                    }
+
+                    return data;
+                },
+            },
+            {
+                data: 'work_status'
+            },
+        ],
+        dom: 'PfrtipB',
+        searchPanes: {
+            initCollapsed: true
+        },
+        buttons: [
+            'copy', 'excel', 'pdf'
+        ]
+    });
+});

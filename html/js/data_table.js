52a8bb3cdee6fc813b9ddf3a3287d2fa412d65b6
csae8092 <peter.andorfer@oeaw.ac.at>
2022-05-26 09:39:19 +0200

    added work_status to datatable (and moved datatables code into dedicated file)

---
+++ html/js/data_table.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    $('#example').DataTable({
+        ajax: 'data/data_table.json',
+        deferRender: true,
+        columns: [{
+                data: 'man_full_sig',
+                render: function (data, type, row) {
+                    if (type === 'display') {
+                        let link = row.man_website_url;
+                        return '<a href="' + link + '">' + data + '</a>';
+                    }
+
+                    return data;
+                },
+            },
+            {
+                data: 'man_title'
+            },
+            {
+                data: 'man_field_status_umschrift'
+            },
+            {
+                data: 'man_pages'
+            },
+            {
+                data: 'man_chapters'
+            },
+            {
+                data: 'work_title',
+                render: function (data, type, row) {
+                    if (type === 'display') {
+                        let link = row.werk_website_url;
+                        return '<a href="' + link + '" target="_blank">' + data + '</a>';
+                    }
+
+                    return data;
+                },
+            },
+            {
+                data: 'work_status'
+            },
+        ],
+        dom: 'PfrtipB',
+        searchPanes: {
+            initCollapsed: true
+        },
+        buttons: [
+            'copy', 'excel', 'pdf'
+        ]
+    });
+});

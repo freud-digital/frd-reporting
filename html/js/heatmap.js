0d2688f205ac47b1039a43783705c4e3d4a5beb6
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-07-12 13:05:21 +0200

    added works_changed heatmap

--- html/js/heatmap.js
+++ html/js/heatmap.js
@@ -1,4 +1,5 @@
 var cal = new CalHeatMap();
+var cal_work = new CalHeatMap();
 
 fetch("https://raw.githubusercontent.com/freud-digital/frd-reporting/gh-pages/data/manifestations_all.json")
     .then(response => response.json())
@@ -12,12 +13,30 @@ fetch("https://raw.githubusercontent.com/freud-digital/frd-reporting/gh-pages/da
             });
             return list;
         }
+        var parser2 = function(data) {
+            var list = {};
+            data.forEach(el => {
+                var work_changed = el.work_changed;
+                list[work_changed] = 1;
+            });
+            return list;
+        }
         cal.init({
+            itemSelector: "#cal-heatmap-man",
             domain: "month",
             start: new Date(2021, 0),
             range: 24,
 	        displayLegend: true,
             data: items,
             afterLoadData: parser
+        });
+        cal_work.init({
+            itemSelector: "#cal-heatmap-works",
+            domain: "month",
+            start: new Date(2021, 0),
+            range: 24,
+	        displayLegend: true,
+            data: items,
+            afterLoadData: parser2
         });
     });

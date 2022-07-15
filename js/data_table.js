8cf0416466ef680e98202243345c14c67d6d13c1
linxOD <linxOD@users.noreply.github.com>
2022-07-15 07:44:49 +0000

    deploy: fda5c7569949ddae5e1b325a21714267e4f95a37

--- js/data_table.js
+++ js/data_table.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
                 render: function (data, type, row) {
                     if (type === 'display') {
                         let hash_id = row.man_id;
-                        return data + '<br />' + '<small>' + hash_id + '</small>'
+                        return data + '<br />' + '<small>' + '<a href="https://www.freud-edition.net/jsonapi/node/manifestation/' + hash_id + '" target="_blank">' + hash_id + '</a>' + '</small>';
                     }
                     return data;
                 },
@@ -38,7 +38,7 @@ $(document).ready(function () {
                     if (type === 'display') {
                         let link = row.werk_website_url;
                         let hash_id = row.work_id;
-                        return '<a href="' + link + '" target="_blank">' + data + '</a><br /><small>' + hash_id + '</small>';
+                        return '<a href="' + link + '" target="_blank">' + data + '</a><br /><small>' + '<a href="https://www.freud-edition.net/jsonapi/node/werk/' + hash_id + '" target="_blank">' + hash_id + '</a>' + '</small>';
                     }
 
                     return data;
